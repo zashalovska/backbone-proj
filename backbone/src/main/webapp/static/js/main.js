8aa29d8e207e32932d0e8dc4128ce247d12f0879
kzashalovska <kzashalovska@connecture.com>
2017-10-12 19:37:21 +0300

    add backbone

--- backbone/src/main/webapp/static/js/main.js
+++ backbone/src/main/webapp/static/js/main.js
@@ -4,15 +4,29 @@ require.config({
         "backbone" : "lib/backbone",
         "underscore" : "lib/underscore",
         "contactView" : "views/contact",
-        "dust" : "lib/dust-full"
+        "dust" : "lib/dust-full",
+        "contacts" : "../templates/contacts.dust"
+    },
+
+  shim: {
+    'backbone': {
+      debts: [
+      'underscore',
+      'jquery'
+      ],
+      exports: 'Contact'
     }
+  }
 });
 
 require(['contactView'], function (ContactView) {
     new ContactView;
 });
 
-define.amd.dust = true;
-require(['dust'], function (dust) {
-  dust.render("");
-});
+/*define.amd.dust = true;
+dust.config.amd = true;
+require(['dust', 'contacts'], function (dust) {
+  dust.render('contacts', { name: "Jupiter" }, function() {
+    console.log("HelloDust!");
+  });*/
+//});
--- backbone/src/main/webapp/static/js/views/contact.js
+++ backbone/src/main/webapp/static/js/views/contact.js
@@ -1,7 +1,29 @@
+
+/*define(function (require) {
+  var Backbone = require('backbone');
+
+  return Backbone.View.extend({
+    template: template,
+    initialize: function () {
+        this.renderData()
+    },
+    renderData: function () {
+
+    }
+
+  });
+});*/
+
+
 define(['backbone'], function (Backbone) {
     var Contact = Backbone.View.extend({
+    el: $('#app'),
         initialize: function () {
-            console.log("Hello!");
+            this.render();
+        },
+        render: function () {
+        console.log('dddd');
+            this.$el.html('<h1>Yey</h1>')
         }
     });
 
